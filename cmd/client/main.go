892c06a0f3688414d9a5b14ab075df5e3a08a615
jxtngb <jestinktr1@gmail.com>
2026-09-24 17:14:24 +0530

    Initialize SOCKS5 server in Ghost Protocol client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/jxtngb/ghost-proxy/pkg/socks5"
+)
 
 func main() {
 	log.Println("Ghost Protocol client starting...")
+
+	server := &socks5.Server{
+		Addr: "127.0.0.1:1080",
+	}
+
+	if err := server.Start(); err != nil {
+		log.Fatalf("SOCKS5 server stopped: %v", err)
+	}
 }
