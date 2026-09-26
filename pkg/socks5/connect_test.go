dc32158f61ed98b10c6ef99c3ed6a92fd0db7f8e
jxtngb <jestinktr1@gmail.com>
2026-09-26 14:48:25 +0530

    Fix indentation in connect_test.go

--- pkg/socks5/connect_test.go
+++ pkg/socks5/connect_test.go
@@ -28,8 +28,8 @@ func TestConnectToTarget(t *testing.T) {
 	}
 
 	conn, err := connectToTarget(request, func(address string) (net.Conn, error) {
-    return net.Dial("tcp", address)
-})
+    	return net.Dial("tcp", address)
+	})
 	if err != nil {
 		t.Fatalf("connectToTarget failed: %v", err)
 	}
